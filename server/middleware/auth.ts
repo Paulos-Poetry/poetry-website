4b5ef7b7d1d12dafac7b3b51151228e71098de32
Sebastian L <99sebastianl@gmail.com>
2025-01-09 18:41:38 -0500

    Enhance admin dashboard for PDF support with 'POEM' autofill and redirect to translations admin. Improve poetry landing and translations dashboard to integrate PDF handling.

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,18 +1,18 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
-    if (!token) {
-        return res.status(401).json({ msg: 'No token, authorization denied' });
-    }
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    try {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
-        req.userId = decoded.userId;
-        next();
-    } catch (error) {
-        res.status(401).json({ msg: 'Token is not valid' });
-    }
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
 };
